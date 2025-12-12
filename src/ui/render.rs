0464a09f473cab4fb43f62d939c13f6373e29a49
Deekline <nicholas.yemets@gmail.com>
2025-12-12 18:23:50 +0100

    Render fn refactoring

--- src/ui/render.rs
+++ src/ui/render.rs
@@ -1,22 +1,76 @@
 use crate::{state::AppState, ui::draw_city_input};
 use ratatui::{
     Frame,
-    layout::{Constraint, Direction, Layout},
+    layout::{Constraint, Direction, Layout, Rect},
+    widgets::{Block, Borders},
 };
 
-pub fn render(frame: &mut Frame, app: &AppState) {
-    let [main_area, _bottom_bar_area] =
-        Layout::vertical([Constraint::Min(0), Constraint::Length(3)]).areas(frame.area());
+pub struct LayoutAreas {
+    pub bottom_bar_area: Rect,
+    pub input_area: Rect,
+    pub history_area: Rect,
+    pub favorites_area: Rect,
+    pub current_area: Rect,
+    pub forecast_area: Rect,
+}
 
-    let [sidebar_area, _content_area] =
+fn compute_layout(area: Rect) -> LayoutAreas {
+    let [main_area, bottom_bar_area] =
+        Layout::vertical([Constraint::Min(0), Constraint::Length(3)]).areas(area);
+
+    let [sidebar_area, content_area] =
         Layout::horizontal([Constraint::Length(50), Constraint::Min(0)]).areas(main_area);
 
     let [input_area, history_area, favorites_area] = Layout::vertical([
         Constraint::Length(3),
-        Constraint::Length(5),
+        Constraint::Length(10),
         Constraint::Min(0),
     ])
     .areas(sidebar_area);
 
-    draw_city_input(frame, input_area, app);
+    let [current_area, forecast_area] =
+        Layout::vertical([Constraint::Min(0), Constraint::Length(10)]).areas(content_area);
+
+    LayoutAreas {
+        bottom_bar_area,
+        input_area,
+        history_area,
+        favorites_area,
+        current_area,
+        forecast_area,
+    }
+}
+
+pub fn render(frame: &mut Frame, app: &AppState) {
+    let areas = compute_layout(frame.area());
+
+    draw_city_input(frame, areas.input_area, app);
+
+    // Placeholder
+    frame.render_widget(
+        Block::default().borders(Borders::ALL).title("History"),
+        areas.history_area,
+    );
+
+    frame.render_widget(
+        Block::default().borders(Borders::ALL).title("Favorites"),
+        areas.favorites_area,
+    );
+
+    frame.render_widget(
+        Block::default().borders(Borders::ALL).title("Current"),
+        areas.current_area,
+    );
+
+    frame.render_widget(
+        Block::default().borders(Borders::ALL).title("Forecast"),
+        areas.forecast_area,
+    );
+
+    frame.render_widget(
+        Block::default()
+            .borders(Borders::ALL)
+            .title("Help / Navigation"),
+        areas.bottom_bar_area,
+    );
 }
